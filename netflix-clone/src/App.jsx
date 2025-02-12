aa4686691b2a6d58c62e4be736493bde045e4c60
Tsegereda Bisrat <koki.abebe@gmail.com>
2025-02-12 08:40:41 -0500

    first commit

---
+++ netflix-clone/src/App.jsx
@@ -0,0 +1,17 @@
+//import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='App'>
+        <h1>Test</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
